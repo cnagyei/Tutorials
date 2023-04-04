9098617a9f01beececbd1ce19bd71a2399ca88bd
cnagyei <cnagyei15@gmail.com>
2023-04-04 04:09:59 +0000

    Incrementing the objects the pointer points to

--- ansi_c/1-ansi.c
+++ ansi_c/1-ansi.c
@@ -7,5 +7,22 @@ int main(void)
 {
 	int x = 10;
 	int *ip;
-	ip
+	int y, z;
+	
+	ip = &x;
+	*ip = *ip + 10;
+	printf("The value of x is: %d\n", *ip);
+
+	/* Unary operators * and & bind more tightly than
+	 * arithmetic opeartors */
+	y = *ip + 1;
+	printf("The value of y is the value of x + 1: %d\n", y);
+
+	z = ++*ip;	/* ++*ip is same as (*ip)++ */
+	printf("The value of z is the value of x incremented: %d\n", z);
+
+	/* Parenthesis in (*ip)++ is important because uninary operators
+	 * like * and ++ associate right to lef */
+		
+	return (0);
 }
